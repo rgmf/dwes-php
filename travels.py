13baaf5504f86283261bdd2783221e7d64e521d8
Román Martínez <rgmf@riseup.net>
2024-02-26 18:53:09 +0100

    Data: python file with list of dictionaries.

---
+++ travels.py
@@ -0,0 +1,26 @@
+travels: list[dict[str, any]] = [
+    {
+        "name": "Viaje a Madrid",
+        "date": "2024-07-07",
+        "locations": ("El retiro", "Museo del prado", "Plaza del sol"),
+        "stars": 4.4
+    },
+    {
+        "name": "Visita a Barcelona",
+        "date": "2023-03-01",
+        "locations": ("Sagrada Familia", "Camp Nou"),
+        "stars": 4.8
+    },
+    {
+        "name": "Explorando París",
+        "date": "2023-09-15",
+        "locations": ("Torre Eiffel", "Louvre", "Montmartre"),
+        "stars": 4.6
+    },
+    {
+        "name": "Aventura en Roma",
+        "date": "2024-01-10",
+        "locations": ("Coliseo", "Foro Romano", "Fontana di Trevi"),
+        "stars": 4.2
+    }
+]
