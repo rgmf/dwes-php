42d4f74966b9ccf2d4a9733baad878d92d3f8329
Román Martínez <rgmf@riseup.net>
2024-02-26 18:49:11 +0100

    First commit.

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+
+<html>
+<head>
+  <title>.: DWES :.</title>
+  <meta charset="utf-i">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <header>
+    <h1>DW(iaj)ES</h1>
+  </header>
+
+  <nav>
+    <ul>
+      <li><a href="/index.php">Home</a></li>
+      <li><a href="/travels/all.php">Viajes</a></li>
+      <li><a href="/travels/best.php">El Mejor Viaje</a></li>
+      <li><a href="/travels/random/create.php">Nuevo Viaje Aleatorio</a></li>
+      <li><a href="/travels/create.php">Nuevo Viaje</a></li>
+      <li><a href="/travels/last/created.php">Último Viaje Añadido</a></li>
+    </ul>
+  </nav>
+
+  <main>
+    <section>
+      <article>
+	<p class="big-center">
+	  Bienveni@s al gestor de <strong>DW(iaj)ES</strong> más espectacular
+	  del mundo, con multitud de opciones.
+	</p>
+      </article>
+    </section>
+  </main>
+  <footer>
+    <p><strong>&copy; Román Martínez</strong></p>
+  </footer>
+</body>
+</html>
